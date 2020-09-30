921371e56fb030af8e79e4a90fe2d499bd4015b6
Animesh Roy <anir0y@MACHomes-MacBook-Pro.local>
2020-09-30 17:03:38 +0530

    init

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'dbadmin');
+define('DB_PASSWORD', 'Passw0rd!20db');
+define('DB_NAME', 'ctfMaster');
+ 
+/* Attempt to connect to MySQL database */
+try{
+    $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
